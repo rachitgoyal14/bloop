41bc0ab58916dae4f6612d2a91965bde5ed71c87
rachitgoyal14 <goyalrachit1410@gmail.com>
2025-12-19 10:31:56 +0530

    feat: add complete missing link game

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from api.flashcards import router as flashcards
 from api.quiz import router as quiz
 from api.play.teach_ai import router as teach_ai
 from api.play.find_mistake import router as find_mistake
+from api.play.complete_missing_link import router as complete_missing_link
 
 app = FastAPI(title="Bloop!")
 
@@ -16,6 +17,7 @@ app.include_router(flashcards)
 app.include_router(quiz)
 app.include_router(teach_ai)
 app.include_router(find_mistake)
+app.include_router(complete_missing_link)
 
 
 @app.get("/")
