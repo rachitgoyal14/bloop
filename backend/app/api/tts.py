411c7774d889ec48f694dc79aef8a315d9a1d86f
Goyam02 <goyamjain02@gmail.com>
2025-12-20 17:27:40 +0530

    add: job_id

--- backend/app/api/tts.py
+++ backend/app/api/tts.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from services.tts_service import text_to_speech
+import uuid
 
 router = APIRouter(prefix="/tts", tags=["TTS"])
 
 @router.post("/answer")
 def speak_answer(text: str):
-    audio_path = text_to_speech(text)
+    job_id = str(uuid.uuid4())
+    audio_path = text_to_speech(text,job_id)
 
     return FileResponse(
         audio_path,
